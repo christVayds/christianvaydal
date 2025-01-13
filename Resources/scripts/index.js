e41423217e5ebf388ea7f55e380d618a0095fc81
christVayds <christianvaydal112@gmail.com>
2025-01-13 23:42:47 +0800

    initialized

---
+++ Resources/scripts/index.js
@@ -0,0 +1,20 @@
+
+var count = 0;
+const array = ['Web Designer', 'Web Developer', 'Software Developer', 'Software Designer', 'Game Developer', 'System Developer', 'Digital Artist'];
+function change() {
+    var imwhat = document.getElementById('imWhat');
+    imwhat.textContent = array[count];
+    if (count >= array.length - 1) {
+        count = 0;
+    } else {
+        count++;
+    }
+}
+
+setTimeout(() => {
+    document.getElementById('intro').classList.remove('view-intro');
+    document.getElementById('about').classList.add('flexed-box-center');
+    document.getElementById('page-nav').classList.add('show');
+
+    setInterval(change, 2000);
+}, 5000);
---
+++ Resources/scripts/jquery.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+
+    function clearTabs(){
+        const tabs = document.getElementsByClassName('nav-item');
+        const pages = document.getElementsByClassName('content');
+
+        for(var i=0;i<tabs.length;i++){
+            tabs[i].classList.remove('active');
+        }
+
+        for(var i=0;i<pages.length;i++){
+            pages[i].classList.remove('flexed-box-center');
+        }
+    }
+
+    function switchPage(page){
+
+        switch(page){
+            case('about_tab'):
+                document.getElementById('about').classList.add('flexed-box-center');
+                document.getElementById('about').scrollTo(0, 0);
+                break;
+            case('skill_tab'):
+                document.getElementById('skills').classList.add('flexed-box-center');
+                break;
+            case('projects_tab'):
+                document.getElementById('projects').classList.add('flexed-box-center');
+                break;
+            default:
+                console.log('page not found');
+                break;
+        }
+    }
+
+    $('.nav-item').click(function(){
+        clearTabs();
+        switchPage(this.id);
+        document.getElementById('all_content').scrollTo({
+            top: 0,
+            bahavior: 'smooth'
+        });
+        this.classList.add('active');
+    });
+});
